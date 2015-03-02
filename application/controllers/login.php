513b560964eaf40417d295c49703068e7b14d4d1
Vikentiy Bobkov <V@vikentiys-mbp.sd.cox.net>
2015-03-02 12:48:44 -0800

    adde justin to rankings, no icon yet

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -257,6 +257,7 @@ class Login extends MY_Controller {
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Call Me Alias','Kashif','Hasan',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('El Presidente','Daniel','Pegg',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Cabo Gus','Gus','Garcia',1500,1500);
+					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Danger Boy','Justin','Levine',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Pendulum','Kyle','Holstein',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Honey Badger','Mark','Cogley',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Beels','Matthew','Beelner',1500,1500);
