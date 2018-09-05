9e888cf938479463923aaa784fde0b56cc1a4879
Victor Bobkov <victorbobkov@Victors-MacBook-Pro.local>
2018-09-04 17:17:56 -0700

    new player

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -297,6 +297,7 @@ class Login extends MY_Controller {
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Dustin Cottle','Dustin','Cottle',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Thuan Chau','Thuan','Chau',1500,1500);
 					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Byron Luk','Byron','Luk',1500,1500);
+					INSERT INTO players(nickname,fname,lname,rating,realtime_rating) VALUES('Joshua Timms','Joshua','Timms',1500,1500);
 					INSERT INTO rank_epoch() VALUES();
 				");
 				$add_players->execute();
