d21c3b01aec5c995fe87add7e522b69f9e0671fd
Victor Bobkov <victorbobkov@Victors-MacBook-Pro.local>
2019-11-18 18:23:08 -0800

    fixage

--- application/config/database.php
+++ application/config/database.php
@@ -47,7 +47,7 @@
 
 $active_group = 'default';
 $dbname = 'pongu';
-$pongu_region = file_get_contents('/root/pongu_region.txt');
+$pongu_region = file_get_contents('/var/www/pongu_region.txt');
 switch($pongu_region) {
 	case "newcastle":
 		$dbname = 'pongu_ncl';
